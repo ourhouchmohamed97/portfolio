22aad7d275172f1d18d35b010e3b12196956db23
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-05-23 11:32:08 +0100

    Feat: add anchor for link in url

--- app/components/websiteDemo.tsx
+++ app/components/websiteDemo.tsx
@@ -58,9 +58,12 @@ export function WebsiteDemo({
                 <svg className="w-4 h-4 text-zinc-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                 </svg>
-                <span className="text-xs dark:text-black truncate">
+                <a 
+                  href="https://www.vervecopilot.com/interview-questions/can-c-string-reverse-be-the-secret-weapon-for-acing-your-next-interview" 
+                  target="_blank"
+                  className="text-xs dark:text-black truncate">
                   {urlSlug}.demo.app
-                </span>
+                </a>
               </div>
             </div>
 
